fbae36121ea1db7ef5a0171cc4c2ea25ff6dd6e2
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-06-30 21:53:02 +0530

    learn about models

---
+++ backend/Models/Stock.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks
+
+namespace backend.Models{
+    public class Stock{
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public string CompanyName { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Change { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ChangePercent { get; set; }
+
+        public decimal LastDividend { get; set; }
+        public string Industry { get; set; }
+        public string MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set;} = new List<Comment>();
+    }
+}
