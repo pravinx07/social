8cce52cc53a33299c500a4307e470f671fa62f8e
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-07-02 11:56:01 +0530

    learn about entity  framework

---
+++ backend/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace  backend.Data{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions):base(dbContextOptions){
+
+        }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- backend/Models/Stock.cs
+++ backend/Models/Stock.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models{
     public class Stock{
