45fff1dc708e7ce9ceec7e29a5ba36d4f176b442
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-07-07 20:30:48 +0530

    learnt about data validations

--- backend/Dtos/Comment/CreateCommentRequestDto.cs
+++ backend/Dtos/Comment/CreateCommentRequestDto.cs
@@ -1,8 +1,17 @@
+
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.Dtos.Comment
 {
     public class CreateCommentRequestDto
     {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(280)]
         public string Title { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(500)]
         public string Content { get; set; }
 
 
